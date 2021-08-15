39acd97e240376a71ba599a9d77b993dec8dfbaf
Abbas <y.abbas.habib@gmail.com>
2021-08-15 12:29:49 +0200

    Fresh start firt Spring build with added dependencies

---
+++ src/main/java/com/spring/HrSystemApplication.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSystemApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HrSystemApplication.class, args);
+    }
+
+}
