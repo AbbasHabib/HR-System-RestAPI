bf447bcf164feacabaaab93341a86e93c2c90c76
Abbas <y.abbas.habib@gmail.com>
2021-08-18 17:28:51 +0200

    added query to final string in interface to improve code readability

--- src/main/java/com/spring/Employee/EmployeeRepository.java
+++ src/main/java/com/spring/Employee/EmployeeRepository.java
@@ -13,17 +13,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>
     List<Employee> findByName(String name);
     List<Employee> findByManager(Employee manager);
 
-    @Query(value = "with recursive cte as (\n" +
-            "  select     id, birth_date, gender, graduation_date, gross_salary, name, net_salary, department_id, manager_id, team_id\n" +
-            "\n" +
-            "  from       employee\n" +
-            "  where      manager_id = :managerId \n" +
-            "  union all\n" +
-            "  select     p.id, p.birth_date, p.gender, p.graduation_date, p.gross_salary, p.name, p.net_salary, p.department_id, p.manager_id, p.team_id\n" +
-            "  from       employee p\n" +
-            "  inner join cte\n" +
-            "          on p.manager_id = cte.id\n" +
-            ")\n" +
-            "select * from cte;\n", nativeQuery = true)
+    @Query(value = Queries.EMPLOYEES_UNDER_MANAGER_RECURSIVE, nativeQuery = true)
     List<Employee> findManagerEmployeesRecursivelyQueried(@Param("managerId") Long managerId);
 }
---
+++ src/main/java/com/spring/Employee/Queries.java
@@ -0,0 +1,18 @@
+package com.spring.Employee;
+
+public interface Queries
+{
+    public static final String EMPLOYEES_UNDER_MANAGER_RECURSIVE = "with recursive cte as (\n" +
+            "  select     id, birth_date, gender, graduation_date, gross_salary, name, net_salary, department_id, manager_id, team_id\n" +
+            "\n" +
+            "  from       employee\n" +
+            "  where      manager_id = :managerId \n" +
+            "  union all\n" +
+            "  select     p.id, p.birth_date, p.gender, p.graduation_date, p.gross_salary, p.name, p.net_salary, p.department_id, p.manager_id, p.team_id\n" +
+            "  from       employee p\n" +
+            "  inner join cte\n" +
+            "          on p.manager_id = cte.id\n" +
+            ")\n" +
+            "select * from cte;";
+
+}
